270b2e494cadf952c4981fd4b40805919d36fdc5
David Pérez <david@bitmodern.com>
2025-04-20 10:25:40 +0000

    feat: handle auth errors Refs: TQ-1702

--- src/logError.ts
+++ src/logError.ts
@@ -1,6 +1,29 @@
 import { logger } from './Logger';
 
 export const logError = (err: any) => {
+  if (!err) return;
+
+  if (
+    // got 401, tried to refresh token, didn't find (ReturnToken.)refresh_token
+    err.id === 'NO_REFRESH_TOKEN' ||
+    err.code === 'NO_REFRESH_TOKEN' ||
+    // got 401, tried to refresh token, refresh token request failed
+    // new SDK only
+    err.id === 'REFRESH_TOKEN_ERROR' ||
+    err.code === 'REFRESH_TOKEN_ERROR' ||
+    // got 401, tried to refresh token, refresh token request failed due to...
+    // old SDK only
+    err.message === 'The refresh token is invalid.' ||
+    // got 401, unknown reason (simply not authorized)
+    err.message === 'Unauthenticated.'
+  ) {
+    logger.error(
+      'There was an authentication issue. Please try logging in again or using valid credentials.',
+      err,
+    );
+    return;
+  }
+
   if (err.error) {
     logger.error(`Status Code ${err.statusCode}`, err.error);
   } else {
